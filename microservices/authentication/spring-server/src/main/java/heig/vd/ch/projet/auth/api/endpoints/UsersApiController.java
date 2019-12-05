de070ae27e5ed9ead2cf9bb47755b2860205a1e6
Burgener francois <francois.burgener@hotmail.fr>
2019-12-05 17:59:36 +0100

    some crud method work (create, patch, get, get with id, delete)

--- microservices/authentication/spring-server/src/main/java/heig/vd/ch/projet/auth/api/endpoints/UsersApiController.java
+++ microservices/authentication/spring-server/src/main/java/heig/vd/ch/projet/auth/api/endpoints/UsersApiController.java
@@ -7,6 +7,7 @@ import heig.vd.ch.projet.auth.api.model.User;
 import heig.vd.ch.projet.auth.repositories.UserRepository;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,44 +28,115 @@ public class UsersApiController implements UserApi {
     @Autowired
     UserRepository userRepository;
 
-    public ResponseEntity<Object> createUser( @ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization,
+    public ResponseEntity<Void> createUser( @ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization,
                                               @ApiParam(value = "", required = true) @Valid @RequestBody User user) {
-        UserEntity newUserEntity = toUserEntity(user);
-        userRepository.save(newUserEntity);
-        Long id = newUserEntity.getId();
 
+        //Verify token
+        if(!authorization.equals("myToken")){
+            //Return an unauthorized status (401)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        //create a user
+        UserEntity newUserEntity = toUserEntity(user);
+
+        //Save the user
+        userRepository.save(newUserEntity);
+
+        // ???
+        Long id = newUserEntity.getId();
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest().path("/{id}")
                 .buildAndExpand(newUserEntity.getId()).toUri();
 
+        //Return a created status (201)
         return ResponseEntity.created(location).build();
     }
 
+
+
     public ResponseEntity<List<User>> getUsers(@ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization) {
         List<User> users = new ArrayList<>();
+
+        //Verify token
+        if(!authorization.equals("myToken")){
+            //Return an unauthorized status (401)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        //Get all users
         for (UserEntity userEntity : userRepository.findAll()) {
             users.add(toUser(userEntity));
         }
+
+        //Return an array of user and an ok status (200)
         return ResponseEntity.ok(users);
     }
 
-    @Override
+
     public ResponseEntity<User> getUserById(@ApiParam(value = "",required=true ) @PathVariable("userId") Long userId,
                                             @ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization) {
 
-        //TODO Here decode token
-        System.out.println(userId);
-        System.out.println(authorization);
+        //Verify token
+        if(!authorization.equals("myToken")){
+            //Return an unauthorized status (401)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
 
-        // Get the user by id
+        //Get the existing user
         UserEntity userEntity = userRepository.findById(userId).get();
         User user = toUser(userEntity);
+
+        //Return the user and ok status (200)
         return ResponseEntity.ok(user);
+    }
+
+    public ResponseEntity<Void> updateUser(@ApiParam(value = "",required=true ) @PathVariable("userId") Long userId,
+                                           @ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization,
+                                           @ApiParam(value = "", required = true) @Valid @RequestBody User user) {
+
+        //Verify token
+        if(!authorization.equals("myToken")){
+            //Return an unauthorized status (401)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        //Get the existing user
+        UserEntity userEntity = userRepository.findById(userId).get();
+
+        //Update the user
+        userEntity.setPassword(user.getPassword());
+
+        //Save the user
+        userRepository.save(userEntity);
+
+        //Return an accept status (202)
+        return ResponseEntity.accepted().build();
+    }
+
+
+    public ResponseEntity<Void> deleteUser(@ApiParam(value = "",required=true ) @PathVariable("userId") Long userId,
+                                           @ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization) {
+
+        //Verify token
+        if(!authorization.equals("myToken")){
+            //Return an unauthorized status (401)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        //Get the existing user
+        UserEntity userEntity = userRepository.findById(userId).get();
+
+        //Delete the user
+        userRepository.delete(userEntity);
+
+        //Return an accept status (202)
+        return ResponseEntity.accepted().build();
     }
 
 
 
-    /*------------------------------------------------------------------------------------------------------------*/
+    /*Utils methods---------------------------------------------------------------------------------------------------*/
     private UserEntity toUserEntity(User user) {
         UserEntity entity = new UserEntity();
         entity.setEmail(user.getEmail());
