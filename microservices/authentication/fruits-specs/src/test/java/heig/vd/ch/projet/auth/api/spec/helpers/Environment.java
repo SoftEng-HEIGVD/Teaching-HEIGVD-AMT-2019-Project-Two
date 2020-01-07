048dcea56d35a7925914d509f0fdae859b91b926
Burgener francois <francois.burgener@heig-vd.ch>
2020-01-07 13:00:08 +0100

    setup cucumber test

--- microservices/authentication/fruits-specs/src/test/java/heig/vd/ch/projet/auth/api/spec/helpers/Environment.java
+++ microservices/authentication/fruits-specs/src/test/java/heig/vd/ch/projet/auth/api/spec/helpers/Environment.java
@@ -1,5 +1,7 @@
 package heig.vd.ch.projet.auth.api.spec.helpers;
 
+import heig.vd.ch.projet.auth.ApiException;
+import heig.vd.ch.projet.auth.ApiResponse;
 import heig.vd.ch.projet.auth.api.DefaultApi;
 
 import java.io.IOException;
@@ -12,10 +14,15 @@ public class Environment {
 
     private DefaultApi api = new DefaultApi();
 
+    public ApiResponse lastApiResponse;
+    public ApiException lastApiException;
+    public boolean lastApiCallThrewException;
+    public int lastStatusCode;
+
     public Environment() throws IOException {
         Properties properties = new Properties();
         properties.load(this.getClass().getClassLoader().getResourceAsStream("environment.properties"));
-        String url = properties.getProperty("io.avalia.fruits.server.url");
+        String url = properties.getProperty("heig.vd.ch.projet.auth.server.url");
         api.getApiClient().setBasePath(url);
 
     }
@@ -24,5 +31,19 @@ public class Environment {
         return api;
     }
 
+    /*public ApiResponse getLastApiResponse() {
+        return lastApiResponse;
+    }
 
+    public ApiException getLastApiException() {
+        return lastApiException;
+    }
+
+    public boolean lastApiCallThrewException(){
+        return lastApiCallThrewException;
+    }
+
+    public int getLastStatusCode() {
+        return lastStatusCode;
+    }*/
 }
--- microservices/authentication/fruits-specs/src/test/java/heig/vd/ch/projet/auth/api/spec/steps/CreationSteps.java
+++ microservices/authentication/fruits-specs/src/test/java/heig/vd/ch/projet/auth/api/spec/steps/CreationSteps.java
@@ -3,11 +3,14 @@ package heig.vd.ch.projet.auth.api.spec.steps;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import heig.vd.ch.projet.auth.api.dto.Roles;
 import heig.vd.ch.projet.auth.api.spec.helpers.Environment;
 import heig.vd.ch.projet.auth.ApiException;
 import heig.vd.ch.projet.auth.ApiResponse;
 import heig.vd.ch.projet.auth.api.DefaultApi;
-import heig.vd.ch.projet.auth.api.dto.Fruit;
+import heig.vd.ch.projet.auth.api.dto.User;
+
+import java.sql.Timestamp;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
@@ -20,47 +23,50 @@ public class CreationSteps {
     private Environment environment;
     private DefaultApi api;
 
-    Fruit fruit;
-
-    private ApiResponse lastApiResponse;
-    private ApiException lastApiException;
-    private boolean lastApiCallThrewException;
-    private int lastStatusCode;
+    User user;
 
     public CreationSteps(Environment environment) {
         this.environment = environment;
         this.api = environment.getApi();
     }
 
-    @Given("^there is a Fruits server$")
-    public void there_is_a_Fruits_server() throws Throwable {
+    @Given("^there is a Users server$")
+    public void there_is_a_Users_server() throws Throwable {
         assertNotNull(api);
     }
 
-    @Given("^I have a fruit payload$")
-    public void i_have_a_fruit_payload() throws Throwable {
-        fruit = new Fruit();
+    @Given("^I have a user payload$")
+    public void i_have_a_user_payload() throws Throwable {
+        user = new User();
+
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+        user.setEmail("francois.burgener" + timestamp.getNanos() + "@heig-vd.ch");
+        user.setFirstname("François");
+        user.setLastname("Burgener");
+        user.setPassword("toto");
+        user.setRole(Roles.ADMIN);
     }
 
-    @When("^I POST it to the /fruits endpoint$")
-    public void i_POST_it_to_the_fruits_endpoint() throws Throwable {
+    @When("^I POST it to the /users endpoint$")
+    public void i_POST_it_to_the_users_endpoint() throws Throwable {
         try {
-            lastApiResponse = api.createFruitWithHttpInfo(fruit);
-            lastApiCallThrewException = false;
-            lastApiException = null;
-            lastStatusCode = lastApiResponse.getStatusCode();
+            environment.lastApiResponse = api.createUserWithHttpInfo("myToken",user);
+            environment.lastApiCallThrewException = false;
+            environment.lastApiException = null;
+            environment.lastStatusCode = environment.lastApiResponse.getStatusCode();
         } catch (ApiException e) {
-            lastApiCallThrewException = true;
-            lastApiResponse = null;
-            lastApiException = e;
-            lastStatusCode = lastApiException.getCode();
+            environment.lastApiCallThrewException = true;
+            environment.lastApiResponse = null;
+            environment.lastApiException = e;
+            environment.lastStatusCode = environment.lastApiException.getCode();
         }
 
     }
 
     @Then("^I receive a (\\d+) status code$")
-    public void i_receive_a_status_code(int arg1) throws Throwable {
-        assertEquals(201, lastStatusCode);
+    public void i_receive_a_status_code(int code) throws Throwable {
+        assertEquals(code, environment.lastStatusCode);
     }
 
 }
