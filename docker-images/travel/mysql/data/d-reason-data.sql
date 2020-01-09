c4fc86fd9f003280c7aa61080f8817bd3e256740
Burgener francois <francois.burgener@heig-vd.ch>
2020-01-09 14:32:52 +0100

    reason data

---
+++ docker-images/travel/mysql/data/d-reason-data.sql
@@ -0,0 +1,16 @@
+USE `travel_api` ;
+INSERT INTO
+    `Reason` (`name`)
+VALUES
+    ('Holiday'),
+    ('Work'),
+    ('Linguistic Stay'),
+    ('Beach'),
+    ('City life'),
+    ('Adventure'),
+    ('Humanitarian'),
+    ('Family'),
+    ('Food'),
+    ('New life');
+
+   
