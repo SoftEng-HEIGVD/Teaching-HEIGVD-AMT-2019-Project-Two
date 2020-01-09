2599ce63bb41562581e4388322b100639d05d0a1
Tiago Póvoa <tiago.quin@gmail.com>
2020-01-09 14:35:21 +0100

    controller country ok

--- microservices/travel/spring-server/src/main/java/heig/vd/ch/projet/travel/api/endpoints/ReasonsApiController.java
+++ microservices/travel/spring-server/src/main/java/heig/vd/ch/projet/travel/api/endpoints/ReasonsApiController.java
@@ -2,17 +2,28 @@ package heig.vd.ch.projet.travel.api.endpoints;
 
 import heig.vd.ch.projet.travel.api.ReasonsApi;
 import heig.vd.ch.projet.travel.api.model.Reason;
+import heig.vd.ch.projet.travel.repositories.ReasonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-07-26T19:36:34.802Z")
 
 @Controller
 public class ReasonsApiController implements ReasonsApi{
+
+    @Autowired
+    ReasonRepository reasonRepository;
+
+    @Autowired
+    HttpServletRequest request;
+
     @Override
     public ResponseEntity<List<Reason>> getReasons() {
+
         return null;
     }
 }
--- microservices/travel/spring-server/src/main/java/heig/vd/ch/projet/travel/repositories/CountryRepository.java
+++ microservices/travel/spring-server/src/main/java/heig/vd/ch/projet/travel/repositories/CountryRepository.java
@@ -2,6 +2,10 @@ package heig.vd.ch.projet.travel.repositories;
 
 import heig.vd.ch.projet.travel.entities.CountryEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.util.Streamable;
+
 
 public interface CountryRepository extends CrudRepository<CountryEntity, Integer> {
+    @Override
+    Streamable<CountryEntity> findAll();
 }
