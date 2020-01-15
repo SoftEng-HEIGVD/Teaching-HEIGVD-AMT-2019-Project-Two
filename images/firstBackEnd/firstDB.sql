7119f05b1592be425a4cda1ac4d98e62ef6203d7
Robel-T <robel.teklehaimanot@heig-vd.ch>
2020-01-15 10:34:35 +0100

    debug2

--- images/firstBackEnd/firstDB.sql
+++ images/firstBackEnd/firstDB.sql
@@ -3,7 +3,7 @@ CREATE DATABASE firstApi;
 USE firstApi;
 
 
-CREATE TABLE `user` (
+CREATE TABLE `user_entity` (
   `email` varchar(255) UNIQUE NOT NULL,
   `username` varchar(255),
   `password` varchar(255),
@@ -15,7 +15,7 @@ CREATE TABLE `user` (
 );
 
 
-INSERT INTO user (`email`,`username`,`password`, `first_name`, `last_name`,`isAdmin`) VALUES ("robel_eteeete@ererer.com", "robelinho", "ok", "robel","test",true);
-INSERT INTO user (`email`,`username`,`password`, `first_name`, `last_name`,`isAdmin`) VALUES ("nair_erere@erer.com", "naireee", "ok", "nair","test",true);
-INSERT INTO user (`email`,`username`,`password`, `first_name`, `last_name`,`isAdmin`) VALUES ("volkan_julien@gameway.com", "waye", "ok", "waye","test",false);
+INSERT INTO user_entity (`email`,`username`,`password`, `first_name`, `last_name`,`isAdmin`) VALUES ("robel_eteeete@ererer.com", "robelinho", "ok", "robel","test",true);
+INSERT INTO user_entity (`email`,`username`,`password`, `first_name`, `last_name`,`isAdmin`) VALUES ("nair_erere@erer.com", "naireee", "ok", "nair","test",true);
+INSERT INTO user_entity (`email`,`username`,`password`, `first_name`, `last_name`,`isAdmin`) VALUES ("volkan_julien@gameway.com", "waye", "ok", "waye","test",false);
 
