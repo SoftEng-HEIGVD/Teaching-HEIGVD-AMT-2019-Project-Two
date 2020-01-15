4e0ac250543a1d6b570d14b6cfcd4902b7a8e325
Robel-T <robel.teklehaimanot@heig-vd.ch>
2020-01-15 10:48:17 +0100

    debug3

--- images/firstBackEnd/firstDB.sql
+++ images/firstBackEnd/firstDB.sql
@@ -9,13 +9,13 @@ CREATE TABLE `user_entity` (
   `password` varchar(255),
   `first_name` varchar(255),
   `last_name` varchar(255),
-  `isAdmin` boolean,
+  `is_admin` boolean,
   CONSTRAINT FK_Coach_username_ID PRIMARY KEY (username)
 
 );
 
 
-INSERT INTO user_entity (`email`,`username`,`password`, `first_name`, `last_name`,`isAdmin`) VALUES ("robel_eteeete@ererer.com", "robelinho", "ok", "robel","test",true);
-INSERT INTO user_entity (`email`,`username`,`password`, `first_name`, `last_name`,`isAdmin`) VALUES ("nair_erere@erer.com", "naireee", "ok", "nair","test",true);
-INSERT INTO user_entity (`email`,`username`,`password`, `first_name`, `last_name`,`isAdmin`) VALUES ("volkan_julien@gameway.com", "waye", "ok", "waye","test",false);
+INSERT INTO user_entity (`email`,`username`,`password`, `first_name`, `last_name`,`is_admin`) VALUES ("robel_eteeete@ererer.com", "robelinho", "ok", "robel","test",true);
+INSERT INTO user_entity (`email`,`username`,`password`, `first_name`, `last_name`,`is_admin`) VALUES ("nair_erere@erer.com", "naireee", "ok", "nair","test",true);
+INSERT INTO user_entity (`email`,`username`,`password`, `first_name`, `last_name`,`is_admin`) VALUES ("volkan_julien@gameway.com", "waye", "ok", "waye","test",false);
 
