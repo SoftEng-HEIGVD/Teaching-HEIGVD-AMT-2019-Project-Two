d9f322c25c04398cdc722a42b6f3e46b2c6eff13
Stephane Selim <stephan.selim@epfl.ch>
2020-01-16 18:22:41 +0100

    establish docker topology and start script

---
+++ execute_project_rest.sh
@@ -0,0 +1,27 @@
+# Build user project with maven and copy the users jar to docker topology
+cd project/spring-server-movies || exit
+mvn clean package install
+cp target/swagger-spring-movies-1.0.0.jar ../../docker/docker-images/backend-movies/
+
+# Go back to project root
+cd ..
+
+# Build movies project with maven and copy movies jar to docker topology
+cd spring-server-users || exit
+mvn clean package install
+cp target/swagger-spring-users-1.0.0.jar ../../docker/docker-images/backend-users/
+
+# Go back to docker topology
+cd ../../docker || exit
+
+# docker compose
+docker-compose down
+docker-compose up --build
+
+# Shut everything down when done
+docker-compose down
+
+# Clean docker (For my memory)
+docker system prune
+docker volume prune
+
